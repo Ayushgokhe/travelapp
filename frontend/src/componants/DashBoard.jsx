909c7e30884b28a03abbc051bd7d9766fbe89767
jagriti008 <123006393+jagriti008@users.noreply.github.com>
2024-06-06 14:28:59 +0530

    search box

--- frontend/src/componants/DashBoard.jsx
+++ frontend/src/componants/DashBoard.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import Navbar from "./Navbar";
-import CardsImg from "./CardsImg";
-import Carousel1 from "./CarouselCard";
-import Footer from "./Footer"
+  import React from "react";
+  import Navbar from "./Navbar";
+  import CardsImg from "./CardsImg";
+  import Carousel1 from "./CarouselCard";
+  import Footer from "./Footer"
+  import SearchForm from './search/SearchForm';
 
-const DashBoard = () => {
-  return (
-    <div>
-      <Navbar />
-      <div class="background-div"></div>
-      <CardsImg/>
-      <Carousel1/>
-      <Footer/>
-    </div> 
-  );   
-};
+  const DashBoard = () => {
+    return (
+      <div>
+        <Navbar />
+        <div class="background-div">
+        <SearchForm/>
+        </div>
+        <CardsImg/>
+        <Carousel1/>
+        <Footer/>
+      </div> 
+    );   
+  };
 
-export default DashBoard;
+  export default DashBoard;
